840a66ca29a27b8ad3934ce7074b347611634925
Gracey0201 <gnwachukwu@clarku.edu>
2025-12-01 17:35:53 -0500

    Update plots_utils.py

--- scr/utils/plots_utils.py
+++ scr/utils/plots_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 from sklearn.decomposition import PCA
 
 # -------------------------------
